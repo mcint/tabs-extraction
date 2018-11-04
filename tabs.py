f061837402300eb831c667e50fd5946e743c396c
Loren McIntyre <lorenpmc@gmail.com>
2018-11-04 01:47:12 -0800

    Add rough outline of util

---
+++ tabs.py
@@ -0,0 +1,30 @@
+import fire
+
+# TODO class Safari(object):
+
+class Firefox(object):
+    import os
+    def path(self):  # last updated time?
+        # return os.path
+        return "~/Library/Application Support/Firefox/Profiles/*.profile/savedsession-state/recovery.jsonlz4" 
+
+    def contains(self,arg):
+        # fetch / extract / $ grep
+        return False
+
+    def fetch(self):
+        self.path()  # self. ??  this. ??  [.] ??
+
+        # Profile
+        # recovery.jsonlz4
+        return 'contents at path()'
+
+    def extract(self, outfile):
+
+        return 'the final file'
+
+
+
+if __name__ == '__main__':
+    fire.Fire(Firefox)
+
