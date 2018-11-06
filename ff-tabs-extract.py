aa21777f1b8a8e4d7bd9590bb78b3f38b7b1667a
Loren McIntyre <lorenpmc@gmail.com>
2018-11-06 10:42:01 -0800

    Complete fetch implementation

--- ff-tabs-extract.py
+++ ff-tabs-extract.py
@@ -9,11 +9,25 @@ import fire
 ## ? ff-tabs [in-name] [out-name]   # assuming jsonlz4, the moz fmt
 
 def fetch():
+    """Copies current version of Firefox recovery backup file and returns path to snapshot"""
     from glob import glob
+    from datetime import datetime
+    from shutil import copyfile
+    import os
+
     paths = glob('/Users/mcint/Library/Application Support/Firefox/Profiles/*/sessionstore-backups/recovery.jsonlz4')
+    srcpath = paths[0]
     
-    # TODO copy to data/raw/  as recovery.jsonlz4.ymd.hms
+    localpath = 'data/raw'
+    timestamp = datetime.strftime(datetime.now(), '%F.%T')
+    tgtfile = '.'.join([os.path.basename(srcpath), timestamp, 'snapshot'])
+    tgtpath = os.path.join(localpath, tgtfile)
+
+    copyfile(srcpath, tgtpath)
+
+    # TODO copy to data/raw/  as recovery.jsonlz4.ymd.hms.snapshot
     # TODO return new path to data/raw/ file
+    return tgtpath
 
 # cp 
 #   ~/Library/Application\ Support/Firefox/profiles/x98wumfy.default/sessionstore-backups/recovery.jsonlz4
@@ -34,4 +48,4 @@ def extract(infile, outfile):
     return 'Succeeded'
 
 if __name__ == '__main__':
-    fire.Fire(extract)
+    fire.Fire()
